f124b102db889635749250473c167dfd77a79a78
mowgood <99533453+mowgood@users.noreply.github.com>
2024-03-01 21:08:40 +0900

    [P/0] 배열 두 배 만들기.java

---
+++ programmers/배열 두 배 만들기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        return Arrays.stream(numbers)
+            .map(n -> n * 2)
+            .toArray();
+    }
+}
