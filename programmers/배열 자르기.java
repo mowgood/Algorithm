09173ff6a0371fdb1a6afe57430bec6307e7f48b
mowgood <99533453+mowgood@users.noreply.github.com>
2024-03-15 00:22:36 +0900

    [P/0] 배열 자르기.java

---
+++ programmers/배열 자르기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        // return Arrays.copyOfRange(numbers, num1, num2+1);
+        
+        return IntStream.rangeClosed(num1, num2)
+            .map(i -> numbers[i])
+            .toArray();
+    }
+}
