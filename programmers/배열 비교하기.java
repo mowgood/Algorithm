1ab5c889baa98ed5d843c64ae04a0d9571e095a4
mowgood <99533453+mowgood@users.noreply.github.com>
2024-04-28 15:23:14 +0900

    [P/0] 배열 비교하기.java

---
+++ programmers/배열 비교하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            return arr1.length > arr2.length ? 1 : -1;
+        }
+        
+        int sum1 = Arrays.stream(arr1).sum();
+        int sum2 = Arrays.stream(arr2).sum();
+        
+        if (sum1 == sum2) return 0;
+        
+        return sum1 > sum2 ? 1 : -1;
+    }
+}
